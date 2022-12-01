f3897ce1aec1a908bdb54e7746603ed306fbe5d3
Tim Bergqvist <tim@piktiv.se>
2022-12-01 08:52:16 +0100

    Added day1

--- src/day_1.rs
+++ src/day_1.rs
@@ -1,7 +1,26 @@
 pub fn a(input: &str) -> i32 {
-  0
+  input.split("\n\n")
+  .map(|i| i.lines())
+  .map(|lines| lines
+      .map(|l| l.parse::<i32>().unwrap())
+      .sum())
+  .max()
+  .unwrap()
 }
 
 pub fn b(input: &str) -> i32 {
-  0
+  let mut list: Vec<i32> = input.split("\n\n")
+  .map(|i| i.lines())
+  .map(|lines| lines
+      .map(|l| l.parse::<i32>().unwrap())
+      .sum())
+  .collect();
+
+  list.sort();
+  
+  list
+    .into_iter()
+    .rev()
+    .take(3)
+    .sum()
 }
